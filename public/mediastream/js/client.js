9c7ee45a17935dda2ceb69bd5d3d87c659437ba9
huanshenyi <txy1226052@gmail.com>
2019-06-16 18:26:35 +0900

    localhostでメディアlabel取得

--- public/mediastream/js/client.js
+++ public/mediastream/js/client.js
@@ -1,9 +1,35 @@
 "use strict"
+//htmlの要素取得用
+var audioSource = document.querySelector("select#audioSource");
+var audioOutput = document.querySelector("select#audioOutput");
+var videoSource = document.querySelector("select#videoSource");
+
 var videoplay = document.querySelector("video#player");
 //video audio
 function gotMediaStream(stream){
     videoplay.srcObject = stream;
+    //全てのメディア設備を返す
+    return navigator.mediaDevices.enumerateDevices();
 }
+
+//ユーザーのメディアの取得ループ
+function gotDevices(deviceInfos){
+    deviceInfos.forEach((deviceinfo)=>{
+
+        var option = document.createElement('option')
+        option.text = deviceinfo.label;
+        option.value = deviceinfo.deviceId; 
+
+        if (deviceinfo.kind === 'audioinput'){
+            audioSource.appendChild(option);
+        }else if(deviceinfo.kind === 'audiooutput'){
+            audioOutput.appendChild(option);
+        }else if(deviceinfo.kind === 'videoinput'){
+            videoSource.appendChild(option);
+        }
+    })
+}
+
 function handlerError(err){
     console.log("getUserMedia error:",err);
 }
@@ -15,5 +41,8 @@ if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia){
        video : true,
        audio : true 
     }
-    navigator.mediaDevices.getUserMedia(constrants).then(gotMediaStream).catch(handlerError);
+    navigator.mediaDevices.getUserMedia(constrants)
+    .then(gotMediaStream)
+    .then(gotDevices)
+    .catch(handlerError);
 }
