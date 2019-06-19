bb472ac60ef431474c0df84b2d9de26132813ec8
huanshenyi <txy1226052>
2019-06-19 17:40:23 +0900

    一部デバイスのパラメタ調整

--- public/mediastream/js/client.js
+++ public/mediastream/js/client.js
@@ -33,16 +33,48 @@ function gotDevices(deviceInfos){
 function handlerError(err){
     console.log("getUserMedia error:",err);
 }
+
+//起動時に実効するもの
+function start(){
+
 if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia){
     console.log("getUserMedia is not supported")
+    return;
 }else{
+    var deviceId = videoSource.value;
     //メディアの取得関連パラメータ
     var constrants = {
-       video : true,
-       audio : true 
+        //videoの大きさも調整可能
+       video : {
+           width: {
+               min:300,
+               max:640
+           },
+           height: 480,
+           //映像のfps数
+        //    frameRate:{
+        //        min:15,
+        //        max:30
+        //    },
+           //facingMode: 'enviroment' 使用するカメラを調整
+           deviceId : deviceId ? deviceId : undefined
+       },
+    //   audio : true 
+        audio : {
+            echoCancellation:true,   //エコー削除
+            //autoGainControl         音声をアップ
+            noiseSuppression:true       //ノイズdown
+            //latency                 ラグの設定,小さいほどラグは少ないが,ネットによって不具合の可能性も大
+            //deviceID                使用するデバイスのidを切替用
+        },
     }
     navigator.mediaDevices.getUserMedia(constrants)
     .then(gotMediaStream)
     .then(gotDevices)
     .catch(handlerError);
+ }
 }
+
+start();
+// //使用するデバイスが変化する時に
+videoSource.onchange = start;
