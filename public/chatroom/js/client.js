43a6c86ed7e450e14d8757f79eb37c1b48720584
huanshenyi <txy1226052@gmail.com>
2019-06-25 08:10:33 +0900

    チャットルーム初期化

---
+++ public/chatroom/js/client.js
@@ -0,0 +1,7 @@
+"use strict"
+var username = document.querySelector('input#username');
+var inputRoom = document.querySelector('input#room');
+var btnConnect = document.querySelector('button#connect');
+var outputArea = document.querySelector('textarea#output');
+var inputArea = document.querySelector('textarea#input');
+var btnSend = document.querySelector('button#send');
