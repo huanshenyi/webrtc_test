062d656f4284fc00405d3299596804d6d267f514
huanshenyi <txy1226052>
2019-07-25 13:38:33 +0900

    soket_serverの改造

--- webserver.js
+++ webserver.js
@@ -6,6 +6,8 @@ var fs = require("fs");
 var express = require("express");
 var serveIndex = require('serve-index');
 
+const USERCOUNT = 3;
+
 //socket.Ioをrequire
 var socketIo = require('socket.io');
 //log
@@ -43,7 +45,7 @@ var io = socketIo.listen(http_server);
 io.sockets.on('connection',(socket)=>{
 
     socket.on('message',(room,date)=>{
-        socket.to(room).emit('message',room,socket.id,date)
+        socket.to(room).emit('message', room, date)
     });
 
     //ルームにjoin
@@ -52,6 +54,15 @@ io.sockets.on('connection',(socket)=>{
       var myRoom = io.sockets.adapter.rooms[room];
       var users = Object.keys(myRoom.sockets).length;
       logger.log('the number of user in room is:'+users);
+      if(user < USERCOUNT) {
+          socket.emit('joined', room, socket.id) //自分以外部屋にいるすべての人にメッセージ
+          if (users > 1) {
+              socket.to(room).emit('otherjoin', room);
+          }
+      }else {
+          socket.leave(room);
+          socket.emit('full', room, socket.id);
+      }
       //socket.emit('joined',room,socket.id);
       //socket.to(room).emit('joined',room,socket.id); //room内に自分以外すべての人にメッセージ
       //io.in(room).emit('joined',room,socket.id); //room内にすべての人にメッセージ
@@ -64,11 +75,13 @@ io.sockets.on('connection',(socket)=>{
         var users = Object.keys(myRoom.sockets).length;
         // users - 1;
         logger.log('the number of user in room is:'+(users-1));
-        socket.leave(room);
-        //socket.emit('joined',room,socket.id);
-        //socket.to(room).emit('joined',room,socket.id); //room内に自分以外すべての人にメッセージ
-        //io.in(room).emit('joined',room,socket.id); //room内にすべての人にメッセージ
-        socket.broadcast.emit('joined',room,socket.id); //自分以外全ての人にメッセージ
+        socket.to(room).emit('bye',room,socket.id);
+        socket.emit('leaved',room,socket.id);
+        // socket.leave(room);
+        // socket.emit('joined',room,socket.id);
+        // socket.to(room).emit('joined',room,socket.id); //room内に自分以外すべての人にメッセージ
+        // io.in(room).emit('joined',room,socket.id); //room内にすべての人にメッセージ
+        // socket.broadcast.emit('joined',room,socket.id); //自分以外全ての人にメッセージ
      });
 });
 http_server.listen(8080,'0.0.0.0');
